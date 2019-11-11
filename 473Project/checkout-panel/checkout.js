56b4c595e0e10775663928ebee3fc96e0cae20c1
laurenamicone <51721796+laurenamicone@users.noreply.github.com>
2019-11-11 09:11:06 -0500

    Update Checkout Form
    HTML and Style Changes

--- 473Project/checkout-panel/checkout.js
+++ 473Project/checkout-panel/checkout.js
@@ -1,41 +1,26 @@
-$(document).ready(function () {
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
 
-  // Radio box border
-  $('.method').on('click', function () {
-    $('.method').removeClass('blue-border');
-    $(this).addClass('blue-border');
-  });
-
-  $('.paypal').on('click', function () {
-    var input = $('.input-fields input');
-  });
-
-  // Validation
-
-  // Multi-Step Form
-  var currentTab = 0; // Current tab is set to be the first tab (0)
-  showTab(currentTab); // Display the crurrent tab
-
-  $('.back-btn').on('click', function () {
-    n = -1;
-    // This function will figure out which tab to display
+function showTab(n) {
+    // This function will display the specified tab of the form ...
     var x = document.getElementsByClassName("tab");
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
-    // if you have reached the end of the form...
-    if (currentTab >= x.length) {
-      // ... the form gets submitted:
-      //document.getElementById("regForm").submit();
-      return false;
+    x[n].style.display = "block";
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
     }
-    // Otherwise, display the correct tab:
-    showTab(currentTab);
-  });
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Complete Purchase";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+}
 
-  $('.next-btn').on('click', function () {
-    n = 1;
+function nextPrev(n) {
     // This function will figure out which tab to display
     var x = document.getElementsByClassName("tab");
     // Exit the function if any field in the current tab is invalid:
@@ -45,62 +30,56 @@ $(document).ready(function () {
     // Increase or decrease the current tab by 1:
     currentTab = currentTab + n;
     // if you have reached the end of the form... :
-    if (currentTab == 1) {
-      //...the form gets submitted:
-      document.getElementById("phone").value.replace(/-/g, "");
-      //document.getElementById("regForm").submit();
-      //return false;
+    if (currentTab >= x.length) {
+        //...the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
     }
     // Otherwise, display the correct tab:
     showTab(currentTab);
-});
-
-function showTab(n) {
-  // This function will display the specified tab of the form...
-  var x = document.getElementsByClassName("tab");
-
-  x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-  if (n == (x.length - 2)) {
-    document.getElementById("nextBtn").innerHTML = "Complete Purchase";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
 }
 
 function validateForm() {
-  // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += "warning";
-      // and set the current valid status to false
-      valid = false;
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false:
+            valid = false;
+        }
     }
-  }
 
-  //Check Email
-  if(currentTab == 0){
-    email = document.getElementById("email").value;
-    var atIndex = email.indexOf('@');
-    var dotIndex = email.indexOf('.');
+    //Check Email
+    if (currentTab == 0) {
+        email = document.getElementById("email").value;
+        var atIndex = email.indexOf('@');
+        var dotIndex = email.indexOf('.');
 
-    if(atIndex == -1 || dotIndex == -1){
-      valid = false;
-      document.getElementById("email").className += "warning";
+        if (atIndex == -1 || dotIndex == -1) {
+            valid = false;
+            document.getElementById("email").className += "warning";
+        }
     }
-  }
-  return valid; // return the valid status
+
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
 }
 
-});
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+}
