c82da8cb89959272148efcae72695d9c826e165a
laurenamicone <51721796+laurenamicone@users.noreply.github.com>
2019-11-11 09:11:55 -0500

    Cart System
    Rough cart system separate from website w/ dummy data

---
+++ 473Project/shopping-cart/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Checkout</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+<!-- JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<div class="container"> 
+   <div class="row">
+      <div class="col-md-5">
+      </div>
+   </div>
+</div>
